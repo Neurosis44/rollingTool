3fdd308b8bdd22fa4a58088e939123d899d78a53
Neurosis <anthony.garreau.44@gmail.com>
2015-07-14 17:57:17 +0200

    "change client et serveur anti cheat"

--- serveur1.js
+++ serveur1.js
@@ -16,8 +16,7 @@ var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
     // Appelé lors d'un jet de dés, retourne le message affiché dans le tchat
     socket.on('roll', function(data) {
-        console.log('roooooll');
-        var jet = roll(data.competenceValue, data.competence, data.bonusMalus, socket.userName);
+        var jet = roll(socket.stats[data.competence], data.competence, data.bonusMalus, socket.userName);
         io.sockets.in(socket.room).emit("roll",{ message: jet });
     });
     // Appelé lors de la connexion d'un nouvel utilisateur
@@ -26,7 +25,8 @@ io.sockets.on('connection', function(socket) {
         
         socket.userName = data.userName;
         socket.room = data.room;
-        
+        console.log(data.stats);
+        socket.stats = data.stats;
         if(!userList[data.room]){
             userList[data.room] = [];
         } 
@@ -39,17 +39,23 @@ io.sockets.on('connection', function(socket) {
     // Appelé lors d'une déconnexion
     socket.on('disconnect', function(data) {
         console.log('disconnection');
-        /*for(var i=0; i < userList[socket.room].length; i++){
-            if(userList[socket.room][i] == socket.userName) userList[socket.room].remove(i);
-        }*/
-        if(userList[socket.room].length > 0)
+ 
+        if(userList[socket.room] && userList[socket.room].length > 0)
             userList[socket.room].splice(userList[socket.room].indexOf(socket.userName),1);
         io.sockets.in(socket.room).emit("refreshUsers",{ userList: userList[socket.room] });
+    });
+    
+     // Appelé lors d'une déconnexion
+    socket.on('user image', function(data) {
+        console.log("image sended.")        
+        io.sockets.in(socket.room).emit('user image', { fileData: data });
+        
     });
 });
 
 // Traitement du jet de dés, calcul du message à émettre aux tchats
 function roll(compValue, comp, bonusMalus, pseudo){
+    console.log('compValue :'+compValue)
     var rand = Math.floor(Math.random() * 100) + 1;
     var totalValue = parseInt(compValue) + parseInt(bonusMalus);
     
